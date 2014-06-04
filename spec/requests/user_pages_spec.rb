9dce7b76f5658fd54b8653dbe0beef4c02465263
Yiming Tang <yimingnju@gmail.com>
2014-06-04 15:14:40 +0800

    Fix a test case coursed by signing in again

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -36,19 +36,20 @@ describe "User Pages" do
         let(:admin) { FactoryGirl.create(:admin) }
 
         before do
+          # Since signed in user can't access sign in page, sign out first
+          click_link ('Sign Out')
           sign_in admin
           visit users_path
         end
 
         it { should have_link('delete', href: user_path(User.first)) }
+        it { should_not have_link('delete', href: user_path(admin)) }
 
         it "should be able to delete another user" do
           expect do
             click_link('delete', match: :first)
           end.to change(User, :count).by(-1)
         end
-
-        it { should_not have_link('delete', href: user_path(admin)) }
       end
     end
   end
