496d53a471e77454420e59783f0feebf038a210e
Yiming Tang <yimingnju@gmail.com>
2014-06-08 16:14:41 +0800

    Add micropost model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :microposts
+
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[a-z0-9-]+\.)+[a-z]{2,})\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
